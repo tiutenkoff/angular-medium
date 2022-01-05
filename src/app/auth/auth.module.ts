46f31734317a6443bcb2c3f244df30244ce2a19f
Alexey <AlekseiTiutenkov@mail.ru>
2022-01-05 17:24:37 +0700

    Add routes for auth.module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register/register.component';
+import { RouterModule } from '@angular/router';
 
-
-
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 @NgModule({
   declarations: [
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AuthModule { }
